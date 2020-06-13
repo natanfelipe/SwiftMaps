b494d1494dc844a3420432db141572ed4f67729b
natanbrito <natanbrito.mtm@outlook.com>
2020-06-13 18:20:25 -0300

    Sydney Opera House is displayed on the map! :tada:

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initMaps()
+    }
+    
+    private func initMaps() {
+        map.setRegion(getRegion(), animated: true)
     }
 
+    private func getRegion() -> MKCoordinateRegion {
+        return MKCoordinateRegion.init(center: getCoordinates(), span: getCoordinateSpan())
+    }
+    
+    private func getCoordinates() -> CLLocationCoordinate2D {
+        let latitude = -33.856565
+        let longitude = 151.215279
+        
+        return CLLocationCoordinate2DMake(latitude, longitude)
+    }
+    
+    //FUNCTION RESPONSABLE FOR DEFINE THE MAP ZOOM
+    private func getCoordinateSpan() -> MKCoordinateSpan {
+        let deltaLatitude: CLLocationDegrees = 0.001
+        let deltaLongitude: CLLocationDegrees = 0.001
+        
+        return MKCoordinateSpan.init(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
+    }
 
 }
 
